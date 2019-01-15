ee220f1e7e05c5ce5f8dfd92d00fb7250016c248
anonOstrich <jesper.kuutti@helsinki.fi>
2019-01-15 19:59:01 +0200

    Restrukturoitu repositorion rakennetta, lisätty unicafe-pohja

---
+++ kurssisovellus/README.md
@@ -0,0 +1,4 @@
+Fullstack-1
+===========
+
+Fullstack-kurssin osa 1
---
+++ unicafe/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <div>Pystytet‰‰n Unicafen uutta toimipistett‰...</div>
+
+ReactDOM.render(<App />, document.getElementById('root')); 
